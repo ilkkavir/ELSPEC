e89675552d0ce97f1f48939d08d1474e6ee48744
Ilkka Virtanen <ilkka.i.virtanen@oulu.fi>
2022-05-20 09:01:09 +0300

    modified the new input routines for hourly files

--- readGUISDAPpar.m
+++ readGUISDAPpar.m
@@ -48,14 +48,18 @@ if isdir(fitdir)
         par=[];
         parstd=[];
         loc=[];
-        return
+        azel = [];
+        I=[];
+        %        return
     end
     
     % number of files
     nf = length(fp);
-    
-    % load the first file to get initial matrix dimensions
-    load(fp{1});
+
+    if nf>0
+        % load the first file to get initial matrix dimensions
+        load(fp{1});
+    end
 else
     mergedfile = true;
     % this must be a merged matlab file
@@ -102,110 +106,150 @@ else
         eval([char(filefields(ii)) '=tmpdata.' char(filefields(ii)) ';']);
     end
 end
-nh = length(r_h);
 
-loc = r_XMITloc;
-
-h = NaN(nh,nf);
-ts = NaN(nf,1);
-te = NaN(nf,1);
-par = NaN(nh,4,nf);
-parstd = NaN(nh,4,nf);
-azel = NaN(nf,2);
-
-for k=1:nf
-
-    if ~mergedfile
-        load(fp{k});
-    else
-        fname = ['data' flist(k).fname((end-11):(end-4))];
-        tmpdata = getfield(load(fitdir,fname),fname);
-        filefields = fieldnames(tmpdata);
-        for ii=1:length(filefields)
-            eval([char(filefields(ii)) '=tmpdata.' char(filefields(ii)) ';']);
+if nf>0
+    
+    nh = length(r_h);
+    
+    loc = r_XMITloc;
+    
+    h = NaN(nh,nf);
+    ts = NaN(nf,1);
+    te = NaN(nf,1);
+    par = NaN(nh,4,nf);
+    parstd = NaN(nh,4,nf);
+    azel = NaN(nf,2);
+    
+    for k=1:nf
+        
+        if ~mergedfile
+            load(fp{k});
+        else
+            fname = ['data' flist(k).fname((end-11):(end-4))];
+            tmpdata = getfield(load(fitdir,fname),fname);
+            filefields = fieldnames(tmpdata);
+            for ii=1:length(filefields)
+                eval([char(filefields(ii)) '=tmpdata.' char(filefields(ii)) ';']);
+            end
         end
-    end
-
-    % start and end times of integration, converted into unix time
-    ts(k) = date2unixtime(r_time(1,:));
-    te(k) = date2unixtime(r_time(2,:));
-
-    % sometimes we do not have fit from all gates
-    nh1 = length(r_h);
-    if nh1 >= nh
-        nh = nh1;
-        ih = 1:nh;
-    else
-        ih=[];
-        for ii = 1:length(r_h)
-            [dummy,ih(ii)] = min(abs(h(:,1)-r_h(ii)));
+        
+        % start and end times of integration, converted into unix time
+        ts(k) = date2unixtime(r_time(1,:));
+        te(k) = date2unixtime(r_time(2,:));
+        
+        % sometimes we do not have fit from all gates
+        nh1 = length(r_h);
+        if nh1 >= nh
+            nh = nh1;
+            ih = 1:nh;
+        else
+            ih=[];
+            for ii = 1:length(r_h)
+                [dummy,ih(ii)] = min(abs(h(:,1)-r_h(ii)));
+            end
         end
+        
+        h(ih,k) = r_h;
+        par(ih,:,k) = r_param(:,[1 2 3 5]);
+        parstd(ih,:,k) = r_error(:,[1 2 3 5]);
+        
+        % remove failed iterations
+        %    failed = (r_status ~= 0) | ( r_res(:,1) > 10 );
+        %    failed = (r_status ~= 0) | ( r_res(:,1) > 30 );
+        failed = ((r_status ~= 0) & (r_status ~=3)) | ( r_res(:,1) > 30 );
+        par(failed,:,k) = NaN;
+        parstd(failed,:,k) = NaN;
+        
+        azel(k,:) = [r_az r_el];
     end
-
-    h(ih,k) = r_h;
-    par(ih,:,k) = r_param(:,[1 2 3 5]);
-    parstd(ih,:,k) = r_error(:,[1 2 3 5]);
-
-    % remove failed iterations
-    %    failed = (r_status ~= 0) | ( r_res(:,1) > 10 );
-    %    failed = (r_status ~= 0) | ( r_res(:,1) > 30 );
-    failed = ((r_status ~= 0) & (r_status ~=3)) | ( r_res(:,1) > 30 );
-    par(failed,:,k) = NaN;
-    parstd(failed,:,k) = NaN;
-
-    azel(k,:) = [r_az r_el];
-end
-
-par(:,3,:) = par(:,3,:) .* par(:,2,:);
-parstd(:,3,:) = parstd(:,2,:).*par(:,3,:) + parstd(:,3,:).*par(:,2,:);
-
-
-% remove other than field-aligned data
-%
-% local field-aligned direction in E region
-[~,~, D, I,~,~,~,~,~,~] = igrfmagm(110000, loc(1), loc(2), decyear(r_time(1,:)));
-FAele = abs(I);
-FAaz = D+180;
-if I<0
-    FAaz = D
-end
-while FAaz < 0
-    FAaz = FAaz + 360;
-end
-while FAaz > 360
-    FAaz = FAaz - 360;
-end
-
-% FAdev degree tolerance to allow changes in field-direction...
-%    rminds = abs(mod(azel(:,1),360) - 187) > 3 | abs(abs(90-azel(:,2))-12.45) > 3;
-rminds = abs(mod(azel(:,1),360) - FAaz) > abs(FAdev) | abs(abs(90-azel(:,2))-(90-FAele)) > abs(FAdev);
-h(:,rminds) = [];
-ts(rminds) = [];
-te(rminds) = [];
-par(:,:,rminds) = [];
-parstd(:,:,rminds) = [];
-azel(rminds,:) = [];
-
-
-% do not accept Ti > 300 K below 100 km altitude or Ti < 50 K anywhere. If Ne < 1e9, keep the electron densities but do not use the temperatures
-[dim1 dim2] = size(h);
-for i1 = 1:dim1
-    for i2 = 1:dim2
-        if h(i1,i2) < 100
-            if  par(i1,2,i2) > 300
+    
+    par(:,3,:) = par(:,3,:) .* par(:,2,:);
+    parstd(:,3,:) = parstd(:,2,:).*par(:,3,:) + parstd(:,3,:).*par(:,2,:);
+    
+    
+    % remove other than field-aligned data
+    %
+    % local field-aligned direction in E region
+    [~,~, D, I,~,~,~,~,~,~] = igrfmagm(110000, loc(1), loc(2), decyear(r_time(1,:)));
+    FAele = abs(I);
+    FAaz = D+180;
+    if I<0
+        FAaz = D
+    end
+    while FAaz < 0
+        FAaz = FAaz + 360;
+    end
+    while FAaz > 360
+        FAaz = FAaz - 360;
+    end
+    
+    % FAdev degree tolerance to allow changes in field-direction...
+    %    rminds = abs(mod(azel(:,1),360) - 187) > 3 | abs(abs(90-azel(:,2))-12.45) > 3;
+    rminds = abs(mod(azel(:,1),360) - FAaz) > abs(FAdev) | abs(abs(90-azel(:,2))-(90-FAele)) > abs(FAdev);
+    h(:,rminds) = [];
+    ts(rminds) = [];
+    te(rminds) = [];
+    par(:,:,rminds) = [];
+    parstd(:,:,rminds) = [];
+    azel(rminds,:) = [];
+    
+    
+    % do not accept Ti > 300 K below 100 km altitude or Ti < 50 K anywhere. If Ne < 1e9, keep the electron densities but do not use the temperatures
+    [dim1 dim2] = size(h);
+    for i1 = 1:dim1
+        for i2 = 1:dim2
+            if h(i1,i2) < 100
+                if  par(i1,2,i2) > 300
+                    par(i1,:,i2) = NaN;
+                    parstd(i1,:,i2) = NaN;
+                end
+            end
+            if par(i1,2,i2) < 50
                 par(i1,:,i2) = NaN;
                 parstd(i1,:,i2) = NaN;
             end
-        end
-        if par(i1,2,i2) < 50
-            par(i1,:,i2) = NaN;
-            parstd(i1,:,i2) = NaN;
-        end
-        if par(i1,1,i2) < 1e9 
-            par(i1,2:end,i2) = NaN;
-            parstd(i1,2:end,i2) = NaN;
+            if par(i1,1,i2) < 1e9 
+                par(i1,2:end,i2) = NaN;
+                parstd(i1,2:end,i2) = NaN;
+            end
         end
     end
+end
+% list possible merged mat files and add their contents
+fmerged = dir(fullfile(fitdir,'GUISDAP*.mat'));
+if ~isempty(fmerged)
+    for k=1:length(fmerged)
+        disp(fullfile(fmerged(k).folder,fmerged(k).name))
+        [hm,tsm,tem,parm,parstdm,locm,azelm,Im] = readGUISDAPpar(fullfile(fmerged(k).folder,fmerged(k).name),FAdev);
+        h = [h,hm];
+        ts = [ts;tsm];
+        te = [te;tem];
+        par = cat(3,par,parm);
+        parstd = cat(3,parstd,parstdm);
+        % loc and locm are identical if found. 
+        if isempty(loc)
+            loc = locm;
+        end
+        azel = [azel;azelm];
+        % I and Im are identical if found.
+        if isempty(I)
+            I = Im;
+        end
+        
+    end
+    [~,iunique] = unique(ts);
+    h = h(:,iunique);
+    ts = ts(iunique);
+    te = te(iunique);
+    par = par(:,:,iunique);
+    parstd = parstd(:,:,iunique);
+
+    [~,isort] = sort(ts);
+    h = h(:,isort);
+    ts = ts(isort);
+    te = te(isort);
+    par = par(:,:,isort);
+    parstd = parstd(:,:,isort);
 end
 
 end
